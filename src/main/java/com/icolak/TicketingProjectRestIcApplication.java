b6db4006ae4221c5a7885129575bb8b2d55fe25d
iskenderColak <iclk84@gmail.com>
2023-02-26 12:13:15 +0300

    Initial commit

---
+++ src/main/java/com/icolak/TicketingProjectRestIcApplication.java
@@ -0,0 +1,13 @@
+package com.icolak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectRestIcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectRestIcApplication.class, args);
+    }
+
+}
